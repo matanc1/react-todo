e2c81a53282c6b9474b0e897251ce37011e6949f
Matan Cohen <matanc1@gmail.com>
2016-11-05 13:30:10 +0200

    package.json: added new npm scripts to allow building and running webpack-dev-server
    webpack.config.js: added css and style loaders and changed the config accordingly

--- webpack.config.js
+++ webpack.config.js
@@ -7,18 +7,23 @@ let config = {
     entry: APP_DIR + '/index.jsx',
     output: {
         path: BUILD_DIR,
+        publicPath: BUILD_DIR,
         filename: 'bundle.js',
-      },
-
+    },
+    devtool: 'source-map',
     module: {
-        loaders: [{
-            test: /\.jsx?$/,
-            exclude: /node_modules/,
-            loader: 'babel-loader',
-          },
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css?$/,
+                exclude: /node_modules/,
+                loaders: ['style-loader', 'css-loader?sourceMap']
+            }
         ],
-      },
-  };
-
-
+    },
+};
 module.exports = config;
